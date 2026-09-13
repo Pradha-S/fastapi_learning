678b37ab23876d1ae6308cff8d71e62ffde330ab
Pradha-S <pradhas08062gmail.com>
2026-09-13 10:14:09 +0530

    Added items and end points

---
+++ Items.py
@@ -0,0 +1,14 @@
+
+
+class Items():
+    id:int
+    name:str
+    price:float
+    quantity:int
+
+    def __init__(self,id,name,price,quantity):
+        self.id=id
+        self.name=name
+        self.price= price
+        self.quantity=quantity
+
--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 #Importing the fastapi and other files
 from fastapi import FastAPI
+from Items import Items
 
 #creating the fastapi application to start working with it
 app=FastAPI()
+
+
+items=[
+    Items(1,"Rice", 240,4),
+    Items(3,"dal", 220,7)
+
+]
+
 
 #used to run the function if user starts from / that is home in url 
 #during get request
@@ -10,5 +19,17 @@ app=FastAPI()
 @app.get("/")
 def greet():
     return {"msg": "WELCOME!!!"}
+
+
+#used to run if we have to search by /home
+
+@app.get("/items")
+def items_all():
+    return items
+
+#use of path parameter
+@app.get("/items/{name}")
+def item_name(name):
+    return {"Item needed": f"{name}"}
 
 
